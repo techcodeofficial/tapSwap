3214d3c418ddc84a70b9e9959c5043b4171d22b3
mohammad <mafrwzh4@gmail.com>
2024-05-29 00:16:31 +0330

    fix earn coin bug ...

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const prompt = require("prompt-sync")();
 const loginParams = prompt("Enter redurect link : ");
 const taps = Number(prompt("enter tabs : "));
-const time = Number(prompt("inter time ms :"));
+const time = Number(prompt("inter time ms : "));
 async function loginer() {
     try {
         let loginData = loginParams
@@ -15,22 +15,10 @@ async function loginer() {
                 { init_data: loginData, referrer: "", bot_key: "app_bot_0" },
                 {
                     headers: {
-                        Accept: "*",
-                        "Accept-Language":
-                            "en-GB,en-US;q=0.9,en;q=0.8,fa;q=0.7",
-                        Connection: "keep-alive",
-                        "Content-Id": "6007480330",
+                        "Content-Id": Math.floor(Math.random()*999999),
                         "Content-Type": "application/json",
-                        Origin: "https://app.tapswap.club",
-                        Referer: "https://app.tapswap.club/",
-                        "Sec-Fetch-Dest": "empty",
-                        "Sec-Fetch-Mode": "cors",
-                        "Sec-Fetch-Site": "cross-site",
                         "User-Agent":
                             "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
-                        "sec-ch-ua": '"Not-A.Brand";v="99", "Chromium";v="124"',
-                        "sec-ch-ua-mobile": "?1",
-                        "sec-ch-ua-platform": '"Android"',
                         "x-app": "tapswap_server",
                         "x-cv": 607
                     }
@@ -58,24 +46,15 @@ refral link : ${token.invite_url}
 coin count : ${token.player.stat.earned}
 ref count : ${token.player.stat.ref_cnt}
   `);
+  let user_id = token.player.id
+  let timestamp = new Date().getTime()
     const config = {
         headers: {
-            Accept: "*",
-            "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8,fa;q=0.7",
             Authorization: `Bearer ${token.access_token}`,
-            Connection: "keep-alive",
-            "Content-Id": "6007480330",
+            "Content-Id": (timestamp * user_id * user_id / user_id) % user_id % user_id,
             "Content-Type": "application/json",
-            Origin: "https://app.tapswap.club",
-            Referer: "https://app.tapswap.club/",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "cross-site",
             "User-Agent":
                 "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36",
-            "sec-ch-ua": '"Not-A.Brand";v="99", "Chromium";v="124"',
-            "sec-ch-ua-mobile": "?1",
-            "sec-ch-ua-platform": '"Android"',
             "x-app": "tapswap_server",
             "x-cv": 607
         }
@@ -83,7 +62,7 @@ ref count : ${token.player.stat.ref_cnt}
     setInterval(async () => {
     const body = {
         taps,
-        time: 1716882699043
+        time: timestamp
     };
     try {
         let { data } = await axios.post(
@@ -93,6 +72,7 @@ ref count : ${token.player.stat.ref_cnt}
         );
         console.log(`earned coin : ${data.player.stat.earned}`);
     } catch (e) {
+        console.log(e)
         console.log("error");
     }
 }, time)
